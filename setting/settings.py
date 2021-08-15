61e0e028681ead5f311d6a35b5e12e689486cbaa
Pritish Roy <pritishkumar.roy@uqconnect.edu.au>
2021-08-15 11:01:06 +1000

    setting secret key from .env file

--- setting/settings.py
+++ setting/settings.py
@@ -12,12 +12,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import logging
 import os
-from pathlib import Path
-
 import pandas as pd
 from django.conf import settings
+from dotenv import load_dotenv
+from pathlib import Path
 
 from setting.config import database_path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a2i^z$+k3i3^+!p8+_lq6^2c#2g(u!3fn8g6m$_=_-%dx)&&yz'
+SECRET_KEY = os.getenv('SECRET_KEY', default='Secret Key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
